3c805797665198cf7e73e328a44fe3e6f8c87ad3
meetrai007 <harmeetsingh77216@gmail.com>
2024-08-01 21:52:48 +0530

    in this commit, fix file heandling bugs an add a option to continue add expence in different category

--- expence_tracker.py
+++ expence_tracker.py
@@ -1,18 +1,28 @@
 import json
 
-expence_data={"feruts":{"apple":45,"banana":66}}
+
+with open ("expence_data.json","r") as f:
+    expence_data=json.load(f)
+    
+
+
+
+# expence_data={"feruts":{"apple":45,"banana":66}}
 total_expence=[]
 
 """feature 1 to add expence ibn a paresent category or create a new category for add expence"""
 choose_category=int(input("1.enter expence in peresent category\n2.enter expence to a new category\nEnter your choice:"))
 if choose_category==1:
-    while True:
+    continuty="y"
+    while continuty != "n":
         print(expence_data.keys())
         category=str(input("Enter category to add expence:"))
 
-        """to add expence in cxpence_data dectionary"""
-        expence_data[category]={(str(input("enter date of perchase"))):(int(input("enter prise you spend")))}
+        """to add expence in expence_data dectionary"""
+        expence_data[category][(str(input("enter date of perchase")))]=(int(input("enter prise you spend")))
+        continuty=str(input("entre y to add more expence or n to exit y/n:"))
 
-        """to store expences json format to a file"""
-        with open("expence.json","w") as file:
-            json.dump(expence_data)
+    """to store expences json format to a file"""
+    with open("expence_data.json","w") as file:
+        json.dump(expence_data, file)
+print(expence_data)
