4a2e472084fd3452bb14ff1aa09930ae418ea4db
meetrai007 <harmeetsingh77216@gmail.com>
2024-08-01 22:05:12 +0530

    add new feature to add category

--- expence_tracker.py
+++ expence_tracker.py
@@ -1,17 +1,12 @@
 import json
 
-
 with open ("expence_data.json","r") as f:
     expence_data=json.load(f)
-    
 
-
-
-# expence_data={"feruts":{"apple":45,"banana":66}}
 total_expence=[]
 
 """feature 1 to add expence ibn a paresent category or create a new category for add expence"""
-choose_category=int(input("1.enter expence in peresent category\n2.enter expence to a new category\nEnter your choice:"))
+choose_category=int(input("1.enter expence in peresent category\n2.Create new category\nEnter your choice:"))
 if choose_category==1:
     continuty="y"
     while continuty != "n":
@@ -19,10 +14,18 @@ if choose_category==1:
         category=str(input("Enter category to add expence:"))
 
         """to add expence in expence_data dectionary"""
-        expence_data[category][(str(input("enter date of perchase")))]=(int(input("enter prise you spend")))
+        expence_data[category][(str(input("enter date of perchase:")))]=(int(input("enter prise you spend:")))
         continuty=str(input("entre y to add more expence or n to exit y/n:"))
 
     """to store expences json format to a file"""
     with open("expence_data.json","w") as file:
         json.dump(expence_data, file)
-print(expence_data)
+
+        
+if choose_category==2:
+    new_cetegory=str(input("Enter new category name:"))
+    expence_data[new_cetegory]={}
+
+    """to store new category in json """
+    with open("expence_data.json","w") as file:
+        json.dump(expence_data, file)
