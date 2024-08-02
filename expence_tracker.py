7f532671ef5509467c62398b532fb0e4243651e2
meetrai007 <harmeetsingh77216@gmail.com>
2024-08-02 15:21:30 +0530

    add more feature to handle error like value error key error

--- expence_tracker.py
+++ expence_tracker.py
@@ -1,56 +1,89 @@
 import json
 
-with open ("expence_data.json","r") as f:
-    expence_data=json.load(f)
+with open ("expense_data.json","r") as f:
+    expense_data=json.load(f)
 
 tcost=[]
-# total_expence=[]
+# total_expense=[]
 
-feature=int(input("\n1.add expence\n2.totel spend in a special category\n3.total spends\nEnter what you do:"))
+while True:
+    while True:
+        try:
+            feature=int(input("\n1.add expense\n2.totel spend in a special category\n3.total spends\n4.exit\nEnter what you do:"))
+            break
+        except Exception as e:
+            print("\n### choose a valid option 1 or 2 or 3 or 4 ###\n")
 
-if feature==1:
-    """feature 1 to add expence ibn a paresent category or create a new category for add expence"""
-    choose_category=int(input("1.enter expence in peresent category\n2.Create new category\nEnter your choice:"))
-    if choose_category==1:
-        continuty="y"
-        while continuty != "n":
-            print(expence_data.keys())
-            category=str(input("Enter category to add expence:"))
 
-            """to add expence in expence_data dectionary"""
-            expence_data[category][(str(input("enter date of perchase:")))]=(int(input("enter prise you spend:")))
-            continuty=str(input("entre y to add more expence or n to exit y/n:"))
+    if feature==1:
+        """feature 1 to add expense ibn a paresent category or create a new category for add expense"""
+        while True:
+            try:
+                choose_category=int(input("1.enter expense in peresent category\n2.Create new category\nEnter your choice:"))
+                break
+            except ValueError:
+                print("\n### choose a valid option 1 or 2 ###\n")
+        
+        if choose_category==1:
+            continuty="y"
+            while continuty != "n":
+                while True:
+                    try:
+                        for categ in expense_data.keys():
+                            print(categ)
+                        category=str(input("Choose category to add expense:"))
+                        """to add expense in expense_data dectionary"""
+                        expense_data[category][(str(input("enter date of perchase:")))]=(int(input("enter prise you spend:")))
+                        continuty=str(input("entre y to add more expense or n to exit y/n:"))
+                        break
+                    except:
+                        print("\n### Enter a valid category showing bellow ###\n")
 
-        """to store expences json format to a file"""
-        with open("expence_data.json","w") as file:
-            json.dump(expence_data, file)
+            """to store expenses json format to a file"""
+            with open("expense_data.json","w") as file:
+                json.dump(expense_data, file)
 
-    if choose_category==2:
-        new_cetegory=str(input("Enter new category name:"))
-        expence_data[new_cetegory]={}
+        if choose_category==2:
+            while True:
+                try:
+                    new_cetegory=str(input("Enter a name for new category:"))
+                    break
+                except:
+                    print("\n### Enter a valid category name try different ###\n")
+            expense_data[new_cetegory]={}
 
-        """to store new category in json """
-        with open("expence_data.json","w") as file:
-            json.dump(expence_data, file)
+            """to store new category in json """
+            with open("expense_data.json","w") as file:
+                json.dump(expense_data, file)
 
-if feature==2:
-    """feature 2 to chect total spend in a special category"""
-    print(expence_data.keys())
-    tspend_category=str(input("Choose category to check total spend:"))
-    print(tspend_category)
-    for k,v in expence_data[tspend_category].items():
-        print(f"\t{k} : {v}")
-        tcost.append(v)
-    print(f"total spend in {tspend_category} is {sum(tcost)}")
-    tcost.clear()
+    if feature==2:
+        """feature 2 to chect total spend in a special category"""
+        while True:
+            try:
+                for categ in expense_data.keys():
+                    print(categ)
+                tspend_category=str(input("Choose category to see total expense:"))
+                print(tspend_category)
+                for k,v in expense_data[tspend_category].items():
+                    print(f"\t{k} : {v}")
+                    tcost.append(v)
+                print(f"total spend in {tspend_category} is {sum(tcost)}")
+                tcost.clear()
+                break
+            except:
+                print("\n### Choose a valid category to see total expense ###\n")
+        
 
-if feature==3:
-    """feature 3 to chect total spend in all categorys"""
-    for key in expence_data.keys():
-        print(key)
-        for k,v in expence_data[key].items():
-            print(f"\t{k} : {v}")
-            tcost.append(v)
-    print(f"total spend in all category is {sum(tcost)}")
-    tcost.clear()
-    
+    if feature==3:
+        """feature 3 to chect total spend in all categorys"""
+        for key in expense_data.keys():
+            print(key)
+            for k,v in expense_data[key].items():
+                print(f"\t{k} : {v}")
+                tcost.append(v)
+        print(f"total spend in all category is {sum(tcost)}")
+        tcost.clear()
+
+    if feature==4:
+        """to exit the application""" 
+        break 
