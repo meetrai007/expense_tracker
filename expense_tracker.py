9501fe273a85587a76876898c6590e5d083a7cff
meetrai007 <harmeetsingh77216@gmail.com>
2024-08-02 20:09:02 +0530

    correct some speling mestacks

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,44 +1,44 @@
 import json
 
-with open ("expence_data.json","r") as f:
-    expence_data=json.load(f)
+with open ("expense_data.json","r") as f:
+    expense_data=json.load(f)
 
 tcost=[]
-# total_expence=[]
+# total_expense=[]
 
-feature=int(input("\n1.add expence\n2.totel spend in a special category\n3.total spends\nEnter what you do:"))
+feature=int(input("\n1.add expense\n2.totel spend in a special category\n3.total spends\nEnter what you do:"))
 
 if feature==1:
-    """feature 1 to add expence ibn a paresent category or create a new category for add expence"""
-    choose_category=int(input("1.enter expence in peresent category\n2.Create new category\nEnter your choice:"))
+    """feature 1 to add expense ibn a paresent category or create a new category for add expense"""
+    choose_category=int(input("1.enter expense in peresent category\n2.Create new category\nEnter your choice:"))
     if choose_category==1:
         continuty="y"
         while continuty != "n":
-            print(expence_data.keys())
-            category=str(input("Enter category to add expence:"))
+            print(expense_data.keys())
+            category=str(input("Enter category to add expense:"))
 
-            """to add expence in expence_data dectionary"""
-            expence_data[category][(str(input("enter date of perchase:")))]=(int(input("enter prise you spend:")))
-            continuty=str(input("entre y to add more expence or n to exit y/n:"))
+            """to add expense in expense_data dectionary"""
+            expense_data[category][(str(input("enter date of perchase:")))]=(int(input("enter prise you spend:")))
+            continuty=str(input("entre y to add more expense or n to exit y/n:"))
 
-        """to store expences json format to a file"""
-        with open("expence_data.json","w") as file:
-            json.dump(expence_data, file)
+        """to store expenses json format to a file"""
+        with open("expense_data.json","w") as file:
+            json.dump(expense_data, file)
 
     if choose_category==2:
         new_cetegory=str(input("Enter new category name:"))
-        expence_data[new_cetegory]={}
+        expense_data[new_cetegory]={}
 
         """to store new category in json """
-        with open("expence_data.json","w") as file:
-            json.dump(expence_data, file)
+        with open("expense_data.json","w") as file:
+            json.dump(expense_data, file)
 
 if feature==2:
     """feature 2 to chect total spend in a special category"""
-    print(expence_data.keys())
+    print(expense_data.keys())
     tspend_category=str(input("Choose category to check total spend:"))
     print(tspend_category)
-    for k,v in expence_data[tspend_category].items():
+    for k,v in expense_data[tspend_category].items():
         print(f"\t{k} : {v}")
         tcost.append(v)
     print(f"total spend in {tspend_category} is {sum(tcost)}")
@@ -46,9 +46,9 @@ if feature==2:
 
 if feature==3:
     """feature 3 to chect total spend in all categorys"""
-    for key in expence_data.keys():
+    for key in expense_data.keys():
         print(key)
-        for k,v in expence_data[key].items():
+        for k,v in expense_data[key].items():
             print(f"\t{k} : {v}")
             tcost.append(v)
     print(f"total spend in all category is {sum(tcost)}")
