ef6737c88357cdb4271929cbb7ed69abdf9a8ae8
meetrai007 <harmeetsingh77216@gmail.com>
2024-09-01 09:51:03 +0530

    work on view chart feature

--- GUI_Version/main.py
+++ GUI_Version/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox, ttk
 import logging,datetime,json
 import matplotlib.pyplot as plt
+from tkcalendar import Calendar, DateEntry
 
 
 """this code for read data from file"""
@@ -15,7 +16,12 @@ except:
 #this code to sore date with expense 
 current_date = str(datetime.date.today())
 
-
+def make_autopct(values):
+    def my_autopct(pct):
+        total = sum(values)
+        val = int(round(pct*total/100.0))
+        return 'total:{v:d} ({p:.1f}%)'.format(p=pct,v=val)
+    return my_autopct
 
 # setting the config level here
 logging.basicConfig(level=logging.INFO)
@@ -116,30 +122,83 @@ def view_expense_root():
 
 """this function for view bar graph and pie chart"""
 def expense_chart_root():
-    category_titles=list(expense_data.keys())
-    category_total_list=[]
-    for category in category_titles:
-        sum=0
-        for items in expense_data[category]:
-            sum+=items["price"]
-        category_total_list.append(float(sum))
-
-    def make_autopct(values):
-                def my_autopct(pct):
-                    total = sum(values)
-                    val = int(round(pct*total/100.0))
-                    return 'total:{v:d} ({p:.1f}%)'.format(p=pct,v=val)
-                return my_autopct
-                    
-    plt.pie(category_total_list,labels=category_titles,autopct='%1.2f%%')
-    plt.show()
     
+    def category_pie():    
+        category_titles=list(expense_data.keys())
+        category_total_list=[]
+        for category in category_titles:
+            sum=0
+            for items in expense_data[category]:
+                sum+=items["price"]
+            category_total_list.append(float(sum))
+        plt.pie(category_total_list,labels=category_titles,autopct=make_autopct(category_total_list))
+        plt.show()
+
+    def example():
+        def print_sel():
+            global startdate
+            startdate=cal2.selection_get()
+            logging.debug(startdate)
+            sdate.config(text=startdate)
+        def print_sel2():
+            global enddate
+            enddate=cal2.selection_get()
+            edate.config(text=enddate)
+            logging.debug(enddate)
+
+        top = Toplevel(mainwindow)
+        top.geometry("300x400+500+100")
+
+        cal2 = Calendar(top,
+                    font="Arial 14", selectmode='day',
+                    cursor="hand1", year=2024, month=8, day=31)
+        cal2.pack(fill="both", expand=True)
+        ttk.Button(top, text="start date", command=print_sel).pack()
+        ttk.Button(top, text="end date", command=print_sel2).pack()
+        # date_wise_bar(cal2.selection_get(),cal2.selection_get())
+
+    
+    def date_wise_bar():
         
+        
+        dayly_spent={}
+        
+        for categoreys in expense_data.keys():
+            for list in expense_data[categoreys]:
+                todaydate=datetime.datetime.strptime(list["date"], "%Y-%m-%d").date()
+                if startdate<=todaydate<=enddate:
+                    dayly_spent[list["date"]]=0
+                    dayly_spent[list["date"]]+=list["price"]
+                    
+        plt.bar(dayly_spent.keys(),dayly_spent.values(),width=.2)
+        plt.title("Total spend record daily")
+        plt.xlabel("Dates of total spends")
+        plt.ylabel("daily total spend amount")
+        plt.show()
+
+    chart_root=Toplevel()
+            
+    chart_root.title("expense tracker")
+    chart_root.geometry("500x600+100+100")
+    chart_root.config(background="#ddd1d3")
+    hadding=Label(chart_root,text="welcome to expense tracker app",bg="blue",fg="white",font=6,width=30).pack(fill=BOTH,ipadx=20,ipady=20)
+    btn1=Button(chart_root,text="category wise chart",width=20,height=2,fg="white",bg="#436af2",font="bold",command=category_pie).place(x=150,y=150)
+    btn2=Button(chart_root,text="enter date here",width=20,height=2,fg="white",bg="#436af2",font="bold",command=example).place(x=150,y=250)
+    sdate=Label(chart_root,text="start date")
+    sdate.place(x=150,y=320)
+    edate=Label(chart_root,text="end date")
+    edate.place(x=250,y=320)
+
+    btn3=Button(chart_root,text="search by date",width=20,height=2,fg="white",bg="#436af2",font="bold",command=lambda:date_wise_bar()).place(x=150,y=350)
+
+
+      
+    
+     
 mainwindow=Tk()
 mainwindow.title("expense tracker")
 mainwindow.geometry("500x600+100+100")
 mainwindow.config(background="#ddd1d3")
-
 hadding=Label(mainwindow,text="welcome to expense tracker app",bg="blue",fg="white",font=6,width=30).pack(fill=BOTH,ipadx=20,ipady=20)
 btn1=Button(mainwindow,text="add expense",width=20,height=2,fg="white",bg="#436af2",font="bold",command=addexpense_only).place(x=150,y=150)
 btn2=Button(mainwindow,text="view expense",width=20,height=2,fg="white",bg="#436af2",font="bold",command=view_expense_root).place(x=150,y=250)
